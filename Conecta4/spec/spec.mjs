7c9635d941d21572d54d93c5dc966360966e3f1b
Ismael B B <ismbou9@alu.edu.gva.es>
2023-12-14 15:13:29 +0100

    Final maybe

--- Conecta4/spec/spec.mjs
+++ Conecta4/spec/spec.mjs
@@ -1,5 +1,6 @@
 import { CrearTablero } from "../src/Reglas/CreateBoard.js";
-import { state } from "../src/conecta4.js";
+import { state } from "../src/gameViews/conecta4.js";
+import {mostrarFicha} from "../src/Reglas/MostrarFichaATirar.js";
 
 
 describe('Conecta 4', function () {
@@ -13,5 +14,12 @@ describe('Conecta 4', function () {
             expect(result.tablero).toEqual(jasmine.any(Array));
 
         }); 
+       
+          it('should initialize overCasilla with a nested array of length 7 filled with 0s', function() {
+            const state = mostrarFicha({});
+            expect(state.overCasilla.length).toBe(1);
+            expect(state.overCasilla[0].length).toBe(7);
+            expect(state.overCasilla[0]).toEqual([0, 0, 0, 0, 0, 0, 0]);
+          });
     });
 });
