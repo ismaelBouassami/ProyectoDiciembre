be0c934f0226857598b3ffa9e55f80ef7b24b28a
Ismael B B <ismbou9@alu.edu.gva.es>
2023-12-14 21:48:15 +0100

    Proyecto Final

--- Conecta4/spec/spec.mjs
+++ Conecta4/spec/spec.mjs
@@ -1,25 +1,40 @@
 import { CrearTablero } from "../src/Reglas/CreateBoard.js";
 import { state } from "../src/gameViews/conecta4.js";
-import {mostrarFicha} from "../src/Reglas/MostrarFichaATirar.js";
+import { mostrarFicha } from "../src/Reglas/MostrarFichaATirar.js";
+import { reiniciarTablero } from "../src/Reglas/RebootGame.js";
+import { comprobarGanador } from "../src/Reglas/Win.js";
+describe("Conecta 4", function () {
+  describe("Board", function () {
+    it("Create Board", function () {
+      const result = CrearTablero(state);
 
-
-describe('Conecta 4', function () {
-    describe('Board',function () {
-        it('Create Board',function () {
-            const result= CrearTablero(state);
-
-            expect(result).toEqual(jasmine.any(Object));
-            expect(result.tablero.length).toEqual(6);
-            expect(result.tablero[0].length).toEqual(7);
-            expect(result.tablero).toEqual(jasmine.any(Array));
-
-        }); 
-       
-          it('should initialize overCasilla with a nested array of length 7 filled with 0s', function() {
-            const state = mostrarFicha({});
-            expect(state.overCasilla.length).toBe(1);
-            expect(state.overCasilla[0].length).toBe(7);
-            expect(state.overCasilla[0]).toEqual([0, 0, 0, 0, 0, 0, 0]);
-          });
+      expect(result).toEqual(jasmine.any(Object));
+      expect(result.tablero.length).toEqual(6);
+      expect(result.tablero[0].length).toEqual(7);
+      expect(result.tablero).toEqual(jasmine.any(Array));
     });
+
+    it("MostrarFicha", function () {
+      const state = mostrarFicha({});
+      expect(state.overCasilla.length).toBe(1);
+      expect(state.overCasilla[0].length).toBe(7);
+      expect(state.overCasilla[0]).toEqual([0, 0, 0, 0, 0, 0, 0]);
+    });
+
+         // Returns "Jugador Rojo ha ganado!" if there is a horizontal line of four red pieces
+    it('Ganador', function() {
+      const statecopy = {
+        tablero: [
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0],
+          [1, 1, 1, 1, 0, 0, 0]
+        ]
+      };
+      const result = comprobarGanador(statecopy);
+      expect(result).toEqual("Jugador Rojo ha ganado!");
+    });
+  });
 });
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # ProyectoDiciembre
  
+# Plataforma de Juegos
+
+## Inicio de Sesión y Registro
+
+Para acceder a la plataforma, utiliza las siguientes credenciales:
+
+**Correo electrónico:** 2ismaelxtv@gmail.com  
+**Contraseña:** ismael
+
+Puedes registrarte si aún no tienes una cuenta. Después de iniciar sesión, serás redirigido a la lista de juegos.
+
+## Lista de Juegos
+
+Desde aquí, podrás ver la lista de juegos disponibles. Si no hay juegos, puedes crear uno haciendo clic en "Crear Juego" y seleccionando "Nuevo Juego". Asegúrate de guardar la instancia para que tus movimientos se registren en nuestra base de datos de Supabase.
+
+## Cargar Juegos
+
+En la lista de juegos, puedes cargar partidas guardadas para continuar jugando.
+
+## Multiplayer (No Implementado)
+
+La función multijugador aún no está disponible.
+
+## Logout y Perfil
+
+Puedes cerrar sesión en cualquier momento y ver o modificar tu perfil haciendo clic en tu nombre de usuario en la barra de navegación.
+
+## Test del Juego
+
+Explora la funcionalidad del juego a través de Jasmine, sobre test del juego, el cual funciona con un tablero de 0, y al clicar se aplican 1 o 2 , respondiendo a rojo o azul al renderizar el tablero.
+
+
+
+Esperamos que disfrutes de la plataforma y agradecemos tu participación. Si tienes alguna pregunta o problema, no dudes en contactarnos.
+
+Atentamente,
+
+Ismael Bouassami Bargach
+
+
+
